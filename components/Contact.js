e27fa3104501974c43441da603f50131a9daddcf
Matthew Domi <70774927+matthewdomi@users.noreply.github.com>
2023-01-05 11:51:38 +0000

    con

--- components/Contact.js
+++ components/Contact.js
@@ -3,7 +3,9 @@ import React from 'react';
 const Contact = () => {
   return (
     <div className="max-w-[1240px] m-auto p-4 h-screen">
-      <h1 className="text-2xl font-bold text-center p-4">We work together</h1>
+      <h1 className="text-2xl font-bold text-center p-4">
+        We work together for Good
+      </h1>
       <form className="max-w-[600px] m-auto">
         <div className="grid grid-cols-2 gap-2">
           <input
